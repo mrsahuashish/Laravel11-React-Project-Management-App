29ef0c999b0aa2b512ae1dfc0425928ac1baa0d2
Ashish Sahu <mrsahuashish@gmail.com>
2024-04-28 11:12:01 +0530

    Solve the migration issue and add some pages

--- database/migrations/2024_04_27_075857_create_projects_table.php
+++ database/migrations/2024_04_27_075857_create_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
-            $table->id();
+           $table->id();
             $table->string('name');
             $table->longText('description')->nullable();
             $table->timestamp('due_date')->nullable();
