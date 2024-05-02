957a7eb41ec561607e0888ab863c240d38c7c13d
Ashish Sahu <mrsahuashish@gmail.com>
2024-05-02 10:07:21 +0530

    Add Project Details

--- app/Http/Requests/StoreprojectRequest.php
+++ app/Http/Requests/StoreprojectRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreprojectRequest extends FormRequest
@@ -11,7 +12,7 @@ class StoreprojectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,11 @@ class StoreprojectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => ['required', 'max:255'],
+            'image' => ['nullable', 'image'],
+            "description" => ['nullable', 'string'],
+            'due_date' => ['nullable', 'date'],
+            'status' => ['required', Rule::in(['pending', 'in_progress', 'completed'])]
         ];
     }
 }
